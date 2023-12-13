e0291eaff8eb539a4a6cab31201ef23aa41dc3e2
mshns <mishanos412@gmail.com>
2023-12-13 15:53:26 +0300

    refactor: separate cron jobs

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
+export { default as cronJobs } from './cronJobs.js';
 export { updateChase } from './updateChase.js';
 export { sendLeaderboard } from './sendLeaderboard.js';
 export { sendFreeroll } from './sendFreeroll.js';
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export { parser } from './parser.js';
 export { bot } from './bot.js';
+export { default as proxyConfig } from './proxyConfig.js';
