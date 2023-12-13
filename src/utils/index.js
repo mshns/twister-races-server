6070668b434d0d317b98f524296fc2e1fa1e6cd4
mshns <mishanos412@gmail.com>
2023-12-14 00:08:39 +0300

    refactor: separate no cors middleware

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
-export { parser } from './parser.js';
 export { bot } from './bot.js';
+export { default as noCORS } from './noCORS.js';
+export { parser } from './parser.js';
 export { default as proxyConfig } from './proxyConfig.js';
