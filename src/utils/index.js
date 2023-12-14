8e4d5bdb6b4f2eb4b9d190b12da9c646e2b90993
mshns <mishanos412@gmail.com>
2023-12-14 14:27:58 +0300

    feat: change current date on main page

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,5 @@
 export { bot } from './bot.js';
+export { default as connectDatabase } from './connectDatabase.js';
 export { default as noCORS } from './noCORS.js';
 export { parser } from './parser.js';
 export { default as proxyConfig } from './proxyConfig.js';
